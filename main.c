b77c51ac16890e7cc236c628f8a09fbc5c3a0b53
jusdal <justin.m.daly@gmail.com>
2023-07-11 15:34:17 +0700

    add fn and struct data

--- main.c
+++ main.c
@@ -3,19 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jdaly <jdaly@student.42.fr>                +#+  +:+       +#+        */
+/*   By: justindaly <justindaly@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/07 19:41:04 by jdaly             #+#    #+#             */
-/*   Updated: 2023/07/07 19:49:16 by jdaly            ###   ########.fr       */
+/*   Updated: 2023/07/11 15:33:55 by justindaly       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "philo.h"
+
+//function to check argument input
+int	check_input(char *av[])
+{
+
+}
+
+//function to initialize structs
+void	init_philos(char *av[], t_data *data)
+{
+	d
+}
+
 int	main(int ac, char *av[])
 {
-	if (ac == 5)
-	{
-		//run philo sim
-	}
+	if (ac < 5 || ac > 6)
+		return (1);
+	if ()
 	else if (ac == 6)
 	{
 		//run philo sim until number of times philo must eat is completed
--- philo.h
+++ philo.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jdaly <jdaly@student.42.fr>                +#+  +:+       +#+        */
+/*   By: justindaly <justindaly@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/07 19:27:52 by jdaly             #+#    #+#             */
-/*   Updated: 2023/07/07 23:20:22 by jdaly            ###   ########.fr       */
+/*   Updated: 2023/07/11 15:23:12 by justindaly       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,26 @@
 
 # include <stdio.h>
 # include <pthread.h>
+# include <bool.h>
 
 typedef struct s_data
 {
+int	total; //total number of philos
+int	die_time; //time to die
+int eat_time; //time to eat
+int	sleep_time; //time to sleep
+int	must_eat; //number of times philo must eat
 
 } t_data;
 
 typedef struct s_philo
 {
-
+int	num; //philo number
+pthread_t		tid; //thread id
+pthread_mutex_t	*fork_l; //right fork (pointer to left fork of adjacent philo)
+pthread_mutex_t fork_r;
+t_data			*data; //pointer to data struct
+bool			finish; //true if eaten must_eat times
 } t_philo;
 
 #endif
